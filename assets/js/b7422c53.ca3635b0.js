da70af3a03fcaf2a0474612ced688226a65f6242
tbrand <tbrand@users.noreply.github.com>
2023-01-23 03:26:43 +0000

    deploy: 58362e694eb5e52bf3b4de178a1fb23f10fd62f7

---
+++ assets/js/b7422c53.ca3635b0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkjp_prototyping_blog=self.webpackChunkjp_prototyping_blog||[]).push([[8112],{7504:o=>{o.exports=JSON.parse('{"label":"congito","permalink":"/blog/tags/congito","allTagsPath":"/blog/tags","count":1}')}}]);
